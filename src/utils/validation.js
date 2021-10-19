558d48264f3404b1a118a8920c158f2821f45e21
akirap3 <akirapf3@gmail.com>
2021-10-19 18:12:03 +0800

    [change] Add function of login with native, FB and Google

---
+++ src/utils/validation.js
@@ -0,0 +1,8 @@
+export const checkEmail = (email) => {
+  const re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  if (!re.test(email)) {
+    alert('您的email 資料格式錯誤');
+    return false;
+  }
+  return true;
+};
