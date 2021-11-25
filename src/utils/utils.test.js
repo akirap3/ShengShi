83451b74ba32dffd2b21dfdfdc0bcb653771c6c3
akirap3 <akirapf3@gmail.com>
2021-11-25 18:12:51 +0800

    [Refactor] Delete unused unit test files

--- src/utils/utils.test.js
+++
@@ -1,9 +0,0 @@
-import { add } from './utils';
-
-describe('add', () => {
-  it('should return 3 if 1= 1, b=2', () => {
-    const inputA = 1;
-    const inputB = 2;
-    expect(add(inputA, inputB)).toEqual(3);
-  });
-});
